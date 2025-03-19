b7085ef669490b6dfc3743d7aa9911c97e416510
Muhammed T <muhammedmhdt@gmail.com>
2025-03-19 13:42:37 +0530

    Fix:all

--- backend/dist/router/carbonRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const carbonController_1 = require("../controller/carbonController");
-const aiController_1 = require("../controller/aiController");
-const router = (0, express_1.Router)();
-router.post("/calculate", carbonController_1.calculateCarbon);
-router.post("/recommendations", aiController_1.getRecommendations);
-exports.default = router;
