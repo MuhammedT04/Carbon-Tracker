df7431d526781bc26d3f226b897fedb7cc6d0b1b
Muhammed T <muhammedmhdt@gmail.com>
2025-03-19 14:10:14 +0530

    Fix-tsconfig-json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "../src/router/carbonRoutes";
+import router from "./router/carbonRoutes";
 
 dotenv.config();
 
